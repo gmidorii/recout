d2b57d453ee9e4be57b42667f86ecb1fbc453bb7
gmidorii <stitow14@gmail.com>
2019-04-14 23:25:46 +0900

    Add graph.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,25 @@
 export default {
-  env: {},
+  env: {
+    baseUrl: "https://pixe.la/v1/users/gmidorii/graphs",
+    graph: "dev-recout",
+    token: process.env.RECOUT_PIXELA_TOKEN
+  },
   head: {
     title: "recout",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js TypeScript project" }
+      {
+        hid: "description",
+        name: "description",
+        content: "Nuxt.js TypeScript project"
+      }
     ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   loading: { color: "#3B8070" },
   css: ["~/assets/css/main.css"],
   build: {},
-  modules: [
-    "@nuxtjs/axios",
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/vuetify"],
   axios: {}
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,46 @@
 <template>
   <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <div class="cards">
-      <Card
-        v-for="person in people"
-        :key="person.id"
-        :person="person"
-      ></Card>
-    </div>
+    <h1 class="header">recout</h1>
+    <v-app>
+      <div class="content">
+        <div class="graph">
+          <img src="https://pixe.la/v1/users/gmidorii/graphs/dev-recout">
+        </div>
+        <form>
+          <v-text-field v-model="output" required></v-text-field>
+          <v-btn color="success" v-on:click="submit">submit</v-btn>
+        </form>
+      </div>
+    </v-app>
   </section>
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
-import { Person } from "~/types";
-import Card from "~/components/Card.vue"
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
+import axios from "axios";
+import { basename } from "path";
 
 @Component({
-  components: {
-    Card
-  }
+  components: {}
 })
 export default class extends Vue {
-  @State people: Person
+  output: string = "";
+
+  public async submit() {
+    const instance = axios.create({
+      baseURL: `${process.env.baseUrl}/${process.env.graph}`,
+      timeout: 5000,
+      headers: {
+        "X-USER-TOKEN": process.env.token
+      }
+    });
+    try {
+      const res = await instance.put("/increment");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 </script>
 
@@ -38,5 +52,10 @@ export default class extends Vue {
 .cards {
   display: flex;
   flex-wrap: wrap;
+}
+
+.content {
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
