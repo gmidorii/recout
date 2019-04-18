357bd7a89ca52af0b0ec34dde8738c1d5d1874cd
gmidorii <stitow14@gmail.com>
2019-04-19 00:10:20 +0900

    Splite directory

---
+++ backend/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Default Port %v", port)
+	}
+
+	log.Printf("Listening on port %v", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprint(w, "Hello World!!")
+}
