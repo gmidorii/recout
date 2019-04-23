0d14ad4ab73e5f2ee5cc9dfa8b2ff1f0d93795aa
gmidorii <stitow14@gmail.com>
2019-04-24 00:36:13 +0900

    Add cors setting.

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/cors v1.0.0
 	github.com/pkg/errors v0.8.0
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
 	go.mercari.io/datastore v1.4.0
--- backend/main.go
+++ backend/main.go
@@ -5,11 +5,20 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"google.golang.org/appengine"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	r.Use(c.Handler)
 	r.Get("/", indexHandler)
 
 	ch := CreateRecoutHandler{
