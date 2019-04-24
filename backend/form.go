246fd03277b32dcd957651235f4d4c419abde9a9
gmidorii <stitow14@gmail.com>
2019-04-25 08:58:02 +0900

    Add factory func for fetch form.

--- backend/form.go
+++ backend/form.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"net/url"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+const defaultLimit = 10
+
 type RecoutForm struct {
 	Message string `json:message`
+}
+
+type FetchForm struct {
+	Limit int
+}
+
+func FactoryFetchForm(values url.Values) (FetchForm, error) {
+	limit, err := getIntValue(values, "limit", defaultLimit)
+	if err != nil {
+		return FetchForm{}, errors.Wrap(err, "failed parse query key=limit")
+	}
+	return FetchForm{Limit: limit}, nil
+}
+
+func getIntValue(values url.Values, key string, defaultValue int) (int, error) {
+	v := values.Get(key)
+	if v == "" {
+		return defaultValue, nil
+	}
+
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed a to i")
+	}
+
+	return i, nil
 }
