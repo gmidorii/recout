43d2658929afe5a0abf5bbce10a7a5fecc67e5ba
gmidorii <stitow14@gmail.com>
2019-04-26 19:36:33 +0900

    Remove unuse code.

--- front/store/index.ts
+++ front/store/index.ts
@@ -1,14 +1,10 @@
-import { RootState, Person } from "~/types";
+import { RootState } from "~/types";
 import { MutationTree, ActionTree } from "vuex";
 
 export const state = (): RootState => ({
   people: []
 });
 
-export const mutations: MutationTree<RootState> = {
-  setPeople(state: RootState, people: Person[]): void {
-    state.people = people;
-  }
-};
+export const mutations: MutationTree<RootState> = {};
 
 export const actions: ActionTree<RootState, RootState> = {};
