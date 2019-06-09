5a87ce1950f4a30f14895c32b7ebbdcda7c7024a
gmidorii <stitow14@gmail.com>
2019-06-09 13:53:18 +0900

    Add logout page.

---
+++ front/pages/logout.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <Header/>
+    <App>
+      <Login></Login>
+    </App>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "nuxt-property-decorator";
+import Header from "~/components/layouts/Header.vue";
+import App from "~/components/layouts/App.vue";
+
+@Component({
+  components: {
+    Header,
+    App
+  }
+})
+export default class extends Vue {}
+</script>
