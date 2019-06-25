9364362a0c53f4bfea46f223cb7328ad24cd2ca6
gmidorii <stitow14@gmail.com>
2019-06-25 17:02:54 +0900

    Add create pixela client #2

--- backend/go.mod
+++ backend/go.mod
@@ -8,11 +8,15 @@ require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/cors v1.0.0
 	github.com/go-chi/render v1.0.1
+	github.com/go-playground/locales v0.12.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
 	github.com/golang/mock v1.3.1
 	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/google/wire v0.2.1
 	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
+	github.com/leodido/go-urn v1.1.0 // indirect
 	github.com/pkg/errors v0.8.0
+	github.com/rs/xid v1.2.1
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
 	go.mercari.io/datastore v1.4.0
 	go.pyspa.org/brbundle v0.0.0-20190515144059-7bf0f84fccc8
@@ -21,4 +25,5 @@ require (
 	golang.org/x/xerrors v0.0.0-20190513163551-3ee3066db522
 	google.golang.org/api v0.3.2 // indirect
 	google.golang.org/appengine v1.5.0
+	gopkg.in/go-playground/validator.v9 v9.29.0
 )
--- backend/infra/pixela/client.go
+++ backend/infra/pixela/client.go
@@ -1,11 +1,14 @@
 package pixela
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/pkg/errors"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 const (
@@ -15,6 +18,7 @@ const (
 
 type Client interface {
 	Increment(userID, token, graph string) error
+	CreateUser(user User) error
 }
 
 type client struct {
@@ -37,5 +41,60 @@ func (c *client) Increment(userID, token, graph string) error {
 		return err
 	}
 	defer resp.Body.Close()
+	return nil
+}
+
+type User struct {
+	Token               string   `validate:"required,get=8,lte=24"`
+	UserName            string   `validate:"required,get=1,lte=32"`
+	AgreeTermsOfService Question `validate:"required"`
+	NotMinor            Question `validate:"required"`
+}
+
+type UserResponse struct {
+	Message   string `json:"message"`
+	IsSuccess bool   `json:"isSuccess"`
+}
+
+type Question string
+
+const (
+	Yes Question = "yes"
+	No  Question = "no"
+)
+
+func (c *client) CreateUser(user User) error {
+	validate := validator.New()
+	if err := validate.Struct(user); err != nil {
+		return err
+	}
+	d, err := json.Marshal(&user)
+	if err != nil {
+		return err
+	}
+
+	req := http.NewRequest("POST", pixelaURL, bytes.NewReader(d))
+	if err != nil {
+		return errors.Wrap(err, "failed new http request")
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code = %v", resp.StatusCode)
+	}
+
+	var decoder json.Decoder
+	var resUser UserResponse
+	if err := decoder.Decode(&resUser); err != nil {
+		return err
+	}
+	if !resUser.IsSuccess {
+		return errors.New("failed create user")
+	}
+
 	return nil
 }
