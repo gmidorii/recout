a46d254e064914bfafe86af7f5ef2c893ed87440
gmidorii <stitow14@gmail.com>
2019-06-28 11:42:54 +0900

    Add footer components.

---
+++ front/components/layouts/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer dark>
+    <div>
+      made by
+      <a href="https://twitter.com/gmidorii" target="_blank">@gmidorii</a>
+    </div>
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
